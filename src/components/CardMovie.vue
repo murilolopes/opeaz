0656857f6d56d85efc86466141703f06d0034158
murilo <murilo@creditcorp.com.br>
2023-12-20 11:19:07 -0300

    adding favorite method

--- src/components/CardMovie.vue
+++ src/components/CardMovie.vue
@@ -3,6 +3,7 @@
     <img :src="movie.Poster" :alt="movie.Title" />
     <h2>{{ movie.Title }}</h2>
     <p>{{ movie.Year }}</p>
+    <button @click="favorite">Favoritar</button>
   </div>
 </template>
 
@@ -13,6 +14,11 @@ export default {
     movie: {
       type: Object,
       required: true,
+    },
+  },
+  methods: {
+    favorite() {
+      this.$store.dispatch("favoriteMovie", this.movie);
     },
   },
 };
